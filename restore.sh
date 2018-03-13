8c66f32b7c2b6f21790c6968788945fec444f4f8
VIDEOSTORIES <CONTACT@VIDEOSTORIES.CO>
2018-03-13 15:46:10 -0300

    restore.sh example banner

--- restore.sh
+++ restore.sh
@@ -3,6 +3,14 @@
 THISPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${THISPATH}/config.sh
 
+INCLUDED=`cat ${THISPATH}/backup.sh | grep include`
+echo "Paths included in the backup.sh:"
+echo "---------"
+echo $INCLUDED
+echo "---------"
+echo "example: ./restore.sh 0D var/www/site.com/htdocs/wp-content/uploads ./uploads/"
+echo "---------"
+
 if [ $# -lt 3 ]; then echo "Usage $0 <date> <file> <restore-to>"; exit; fi
 
 duplicity \
